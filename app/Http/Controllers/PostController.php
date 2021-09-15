cadc8cd86a172d4b4a84fcb958fd6cb023ea165a
keito-hoshi <a3197263@mi.sanno.ac.jp>
2021-09-15 17:43:54 +0900

    [add]記事一覧画面追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+
 class PostController extends Controller
 {
-    /**
-     * Post一覧を表示する
-     * 
-     * @param Post Postモデル
-     * @return array Postモデルリスト
-     */
     public function index(Post $post)
     {
-        return $post->get();
+        return view('index')->with(['posts' => $post->get()]);
     }
 }
